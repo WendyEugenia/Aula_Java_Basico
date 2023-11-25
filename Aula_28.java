743fbaae71b6ad3250aa65a7185e28ffbd7d95b4
WendyEugenia <wendy.t.n@hotmail.com>
2023-11-25 01:31:27 -0300

    add condição

---
+++ Aula_28.java
@@ -0,0 +1,20 @@
+package Atividades;
+
+import java.util.Scanner;
+
+public class Aula_28 {
+	//Condição
+	
+	//condição ? caso verdadeiro: caso falso
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		int maior;
+		
+		maior = (a<b) ? 500: 1000;
+		
+		System.out.println(maior);
+	}
+}
