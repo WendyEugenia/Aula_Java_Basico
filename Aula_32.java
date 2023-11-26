79c9c1c3cd6fba626c581895a55ae7a8471bea4b
WendyEugenia <wendy.t.n@hotmail.com>
2023-11-26 00:20:28 -0300

    Atividades

--- Aula_32.java
+++ Aula_32.java
@@ -5,6 +5,9 @@ public class Aula_32 {
 	public static void main(String[] args) {
 		
 		int i;
+		//outra maneira de informar os valores do vetor e com virgula
+		//int[] x = { 155, 1, 5454,6,84,9};
+		
 		int[] x = new int[10];
 							//x=local  3=o numero que deve ser mudadp nas quantidades  
 		java.util.Arrays.fill(x, 2, 8, 3);//o fill faz mudar os quaais quero mudança
@@ -13,7 +16,5 @@ public class Aula_32 {
 			System.out.println(x[i]);
 		}
 		
-		
-
 	}
 }
