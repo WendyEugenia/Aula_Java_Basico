db8a588cd58d8f92839156ff4619f519ab1f6a17
WendyEugenia <wendy.t.n@hotmail.com>
2023-11-26 15:49:05 -0300

    Atividades

--- Aula_32.java
+++ Aula_32.java
@@ -1,20 +1,24 @@
 package Atividades;
 
 public class Aula_32 {
-
+	// iniciar vetores com chaves
 	public static void main(String[] args) {
 		
-		int i;
+		//int i;	
 		//outra maneira de informar os valores do vetor e com virgula
 		//int[] x = { 155, 1, 5454,6,84,9};
 		
 		int[] x = new int[10];
 							//x=local  3=o numero que deve ser mudadp nas quantidades  
 		java.util.Arrays.fill(x, 2, 8, 3);//o fill faz mudar os quaais quero mudança
+			
+		int vetor[] = { 3,6,2,1,8,4};
+
 		
-		for(i = 0; i < x.length; ++i) {
-			System.out.println(x[i]);
+		for(int i = 0; i < vetor.length; ++i) {
+			System.out.println(vetor[i] + " ");
 		}
 		
 	}
 }
+//
